f41e009e4747c100bd9e663fd5880e31e5922d14
shafiranaya <shfrnaya@gmail.com>
2021-02-13 21:15:39 +0700

    added isCellOccupied, getPositionWormMinHealth, dll

--- src/main/java/za/co/entelect/challenge/Bot.java
+++ src/main/java/za/co/entelect/challenge/Bot.java
@@ -13,6 +13,7 @@ public class Bot {
     private Random random;
     private GameState gameState;
     private Opponent opponent;
+    private MyPlayer player;
     private MyWorm currentWorm;
 
     public Bot(Random random, GameState gameState) {
@@ -23,15 +24,13 @@ public class Bot {
     }
 
     private MyWorm getCurrentWorm(GameState gameState) {
-        return Arrays.stream(gameState.myPlayer.worms)
-                .filter(myWorm -> myWorm.id == gameState.currentWormId)
-                .findFirst()
-                .get();
+        return Arrays.stream(gameState.myPlayer.worms).filter(myWorm -> myWorm.id == gameState.currentWormId)
+                .findFirst().get();
     }
-//    Cek x1 between x2 sama x3 apa ga
+    // Cek x1 between x2 sama x3 apa ga
 
-    private boolean between(int x1, int x2, int x3){
-        if(x2 > x3){
+    private boolean between(int x1, int x2, int x3) {
+        if (x2 > x3) {
             return x1 >= x3 && x1 <= x2;
         } else {
             return x1 <= x3 && x1 >= x2;
@@ -39,23 +38,24 @@ public class Bot {
     }
 
     public Command run() {
-//        Kalo ada enemy yang bisa ditembak -> ditembak
+        // Kalo ada enemy yang bisa ditembak -> ditembak
         Worm enemyWorm = getFirstWormInRange();
         if (enemyWorm != null && enemyWorm.health > 0) {
             Direction direction = resolveDirection(currentWorm.position, enemyWorm.position);
             return new ShootCommand(direction);
         }
 
-//        Nyari worm enemy yang paling deket
+        // Nyari worm enemy yang paling deket
         Worm closestEnemy = opponent.worms[1];
         double closestDistance = 50;
-        for (Worm enemyWorms : opponent.worms){
+        for (Worm enemyWorms : opponent.worms) {
             double accumDistance = 0;
-            for(Worm myWorm : gameState.myPlayer.worms){
-                accumDistance += euclideanDistance(myWorm.position.x, myWorm.position.y, enemyWorms.position.x, enemyWorms.position.y);
+            for (Worm myWorm : gameState.myPlayer.worms) {
+                accumDistance += euclideanDistance(myWorm.position.x, myWorm.position.y, enemyWorms.position.x,
+                        enemyWorms.position.y);
             }
 
-            if(accumDistance < closestDistance){
+            if (accumDistance < closestDistance) {
                 closestDistance = accumDistance;
                 closestEnemy = enemyWorms;
             }
@@ -74,19 +74,21 @@ public class Bot {
         return new DoNothingCommand();
     }
 
-//    Dari surrounding cells, dipilih cell buat move/dig
-    private Cell chooseCell(List<Cell> surroundingCells, int destinationX, int destinationY){
+    // Dari surrounding cells, dipilih cell buat move/dig
+    private Cell chooseCell(List<Cell> surroundingCells, int destinationX, int destinationY) {
         boolean diagonalChosen = false;
 
         Cell chosenCell = surroundingCells.get(0);
         int i = 0;
 
-//        Mencari cell yang bisa membawa ke cell tujuan
-        while(i < surroundingCells.size() && !diagonalChosen){ //Karena move diagonal bakal paling efektif, kalo bisa diagonal yang dipilih yang diagonal.
+        // Mencari cell yang bisa membawa ke cell tujuan
+        while (i < surroundingCells.size() && !diagonalChosen) { // Karena move diagonal bakal paling efektif, kalo bisa
+                                                                 // diagonal yang dipilih yang diagonal.
             Cell currentCell = surroundingCells.get(i);
-            if(between(currentCell.x, destinationX, currentWorm.position.x) && between(currentCell.y, destinationY, currentWorm.position.y)){
+            if (between(currentCell.x, destinationX, currentWorm.position.x)
+                    && between(currentCell.y, destinationY, currentWorm.position.y)) {
                 chosenCell = currentCell;
-                if(currentCell.x != currentWorm.position.x && currentCell.y != currentWorm.position.y){
+                if (currentCell.x != currentWorm.position.x && currentCell.y != currentWorm.position.y) {
                     diagonalChosen = true;
                 }
             }
@@ -97,11 +99,8 @@ public class Bot {
 
     private Worm getFirstWormInRange() {
 
-        Set<String> cells = constructFireDirectionLines(currentWorm.weapon.range)
-                .stream()
-                .flatMap(Collection::stream)
-                .map(cell -> String.format("%d_%d", cell.x, cell.y))
-                .collect(Collectors.toSet());
+        Set<String> cells = constructFireDirectionLines(currentWorm.weapon.range).stream().flatMap(Collection::stream)
+                .map(cell -> String.format("%d_%d", cell.x, cell.y)).collect(Collectors.toSet());
 
         for (Worm enemyWorm : opponent.worms) {
             String enemyPosition = String.format("%d_%d", enemyWorm.position.x, enemyWorm.position.y);
@@ -126,7 +125,8 @@ public class Bot {
                     break;
                 }
 
-                if (euclideanDistance(currentWorm.position.x, currentWorm.position.y, coordinateX, coordinateY) > range) {
+                if (euclideanDistance(currentWorm.position.x, currentWorm.position.y, coordinateX,
+                        coordinateY) > range) {
                     break;
                 }
 
@@ -162,8 +162,7 @@ public class Bot {
     }
 
     private boolean isValidCoordinate(int x, int y) {
-        return x >= 0 && x < gameState.mapSize
-                && y >= 0 && y < gameState.mapSize;
+        return x >= 0 && x < gameState.mapSize && y >= 0 && y < gameState.mapSize;
     }
 
     private Direction resolveDirection(Position a, Position b) {
@@ -186,4 +185,91 @@ public class Bot {
 
         return Direction.valueOf(builder.toString());
     }
+
+    private boolean isDirt(Cell c) {
+        return (c.type == CellType.DIRT);
+    }
+
+    private boolean isDeepSpace(Cell c) {
+        return (c.type == CellType.DEEP_SPACE);
+    }
+
+    // Cek apakah cell diisi oleh worm lain
+    private boolean isCellOccupied(Cell c) {
+        Worm[] opponentWorms = opponent.worms;
+        Worm[] playerWorms = player.worms;
+        int i = 0;
+        int j = 0;
+        boolean foundOpponentWorm = false;
+        boolean foundPlayerWorm = false;
+        while ((i < opponentWorms.length) && (!foundOpponentWorm)) {
+            if ((opponentWorms[i].position.x == c.x) && (opponentWorms[i].position.y == c.y)) {
+                foundOpponentWorm = true;
+            } else {
+                i++;
+            }
+        }
+        while ((j < playerWorms.length) && (!foundPlayerWorm)) {
+            if ((playerWorms[j].position.x == c.x) && (playerWorms[j].position.y == c.y)) {
+                foundPlayerWorm = true;
+            } else {
+                j++;
+            }
+        }
+        return (foundOpponentWorm && foundPlayerWorm);
+    }
+
+    // Cek apakah cell surrounded by dirt
+    private boolean isSurroundedByDirt(Cell c) {
+        List<Cell> surrounding = getSurroundingCells(c.x, c.y);
+        int i = 0;
+        boolean isAllDirt = true;
+        while (i < surrounding.size() && (!isAllDirt)) {
+            if (!isDirt(surrounding.get(i))) {
+                isAllDirt = false;
+            } else {
+                i++;
+            }
+        }
+        return isAllDirt;
+    }
+
+    // Cek apakah cell surrounded by deep space
+    private boolean isSurroundedByDeepSpace(Cell c) {
+        List<Cell> surrounding = getSurroundingCells(c.x, c.y);
+        int i = 0;
+        boolean isAllDeepSpace = true;
+        while (i < surrounding.size() && (!isAllDeepSpace)) {
+            if (!isDeepSpace(surrounding.get(i))) {
+                isAllDeepSpace = false;
+            } else {
+                i++;
+            }
+        }
+        return isAllDeepSpace;
+    }
+
+    // Cari posisi worm lawan yang health nya minimum
+    private Position getPositionWormMinHealth() {
+        Worm[] opponentWorms = opponent.worms;
+        int min = opponentWorms[0].health;
+        int i;
+        for (i = 0; i < opponentWorms.length; i++) {
+            if (opponentWorms[i].health < min) {
+                min = opponentWorms[i].health;
+            }
+        }
+        return (opponentWorms[i].position);
+    }
+
+    /*
+     * TODO Fungsi kelayakan, pengecekan untuk tidak melakukan invalid command.
+     * bingung parameternya hehe, yang penting gunain isCellOccupied, isDirt,
+     * isDeepSpace Worms gabisa gerak ke cell yg ga adjacent -> udah ada
+     * getSurroundingCells Worms cannot move to cells occupied by another worm Worms
+     * cannot move to dirt or deep space cells
+     */
+    // private boolean isMoveValid
+
 }
+// cd src/main/java/za/co/entelect/challenge
